17123eef454203e0e862292cba4c4e4fcaeb572d
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-10-31 22:46:36 -0600

    Subiendo jframe from sin funcionalidad y prueba de main

---
+++ Examenes/Parcial_1/hilosAnillo/src/main/java/App.java
@@ -0,0 +1,19 @@
+
+import com.escom.redes2.hilosanillo.Vista.Inicio;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author aarongarcia
+ */
+public class App {
+    public static void main(String[] args) 
+    {
+        System.out.println("Hola mundo");
+    }
+}
