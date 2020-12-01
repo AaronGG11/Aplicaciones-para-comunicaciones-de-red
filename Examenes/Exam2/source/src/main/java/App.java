d801a50f1d66b966fa4ebac048b8f90d586beac3
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-11-30 19:32:35 -0600

    Probando path de imagenes y colocando imagen a tamaño de boton

--- Examenes/Exam2/source/src/main/java/App.java
+++ Examenes/Exam2/source/src/main/java/App.java
@@ -1,9 +1,25 @@
 import Board.Board;
 
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
 public class App {
     public static void main(String[] args)
     {
+        // Path de carpeta de imagenes
+        StringBuilder images_path = new StringBuilder();
+        images_path.append("..");
+        images_path.append(File.separator);
+        images_path.append("images");
+        images_path.append(File.separator);
+        images_path.append("fondo.jpg");
+
         Board tablero = new Board("1234");
         tablero.setVisible(true);
+
+        ImageIcon foto = new ImageIcon(images_path.toString());
+        Icon icono = new ImageIcon(foto.getImage().getScaledInstance(tablero.btn_11.getWidth(),tablero.btn_11.getHeight(), Image.SCALE_DEFAULT));
+        tablero.btn_11.setIcon(icono);
     }
 }
