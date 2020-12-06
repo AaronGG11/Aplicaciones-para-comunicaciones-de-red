c586893e862bc68c259d92bc3af30587e747a4e5
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-12-06 15:28:33 -0600

    subiendo ejemplo sockets tcp no bloquenante

--- Examenes/Exam2/source/src/main/java/model/Client.java
+++ Examenes/Exam2/source/src/main/java/model/Client.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 
 public class Client {
     public static void main(String[] args) {
+        // Sockets TCP (de flujo) NO BLOQUEANTES
+
         String[] list = {"Pareja", "Solitario"};
         JComboBox jcb = new JComboBox(list);
         JOptionPane.showMessageDialog( null, jcb, "Modo de juego ", JOptionPane.QUESTION_MESSAGE);
--- Examenes/Exam2/source/src/main/java/model/Server.java
+++ Examenes/Exam2/source/src/main/java/model/Server.java
@@ -2,6 +2,6 @@ package model;
 
 public class Server {
     public static void main(String[] args) {
-        
+        // Sockets TCP (de flujo) NO BLOQUEANTES
     }
 }
