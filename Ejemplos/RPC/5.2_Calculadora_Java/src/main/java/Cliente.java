48ff14e0f4a143b78b111cb65da5e5f8b4669099
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-12-07 11:26:47 -0600

    Subiendo ejemplo de RMI JAVA CALCULADORA

--- Ejemplos/RPC/5.2_Calculadora_Java/src/main/java/Cliente.java
+++ Ejemplos/RPC/5.2_Calculadora_Java/src/main/java/Cliente.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Cliente {
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Cliente {
+    private Cliente() {}
+    public static void main(String[] args) {
+        String host = (args.length < 1) ? null : args[0];
+        try {
+            Registry registry = LocateRegistry.getRegistry(host);
+            //también puedes usar getRegistry(String host, int port)
+            Calculadora stub = (Calculadora) registry.lookup("Calculadora");
+
+            int x=5,y=4;
+            int response_suma = stub.suma(x,y);
+            int response_resta = stub.resta(x,y);
+            int response_multiplicacion = stub.multiplicacion(x,y);
+            float response_division = stub.division(x,y);
+
+            System.out.println("respuesta sumar "+x+" y "+y+" : " + response_suma);
+            System.out.println("respuesta restar "+x+" y "+y+" : " + response_resta);
+            System.out.println("respuesta multiplicar "+x+" y "+y+" : " + response_multiplicacion);
+            System.out.println("respuesta dividir "+x+" y "+y+" : " + response_division);
+        } catch (Exception e) {
+            System.err.println("Excepción del cliente: " +e.toString());
+            e.printStackTrace();
+        }
+    }
 }
--- Ejemplos/RPC/5.2_Calculadora_Java/src/main/java/Servidor.java
+++ Ejemplos/RPC/5.2_Calculadora_Java/src/main/java/Servidor.java
@@ -1,2 +1,53 @@
-package PACKAGE_NAME;public class Servidor {
+import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Servidor implements Calculadora {
+    public Servidor() {}
+
+    public int suma(int a, int b) {
+        return a+b;
+    }
+
+    @Override
+    public int resta(int a, int b) throws RemoteException {
+        return a-b;
+    }
+
+    @Override
+    public int multiplicacion(int a, int b) throws RemoteException {
+        return a*b;
+    }
+
+    @Override
+    public float division(int a, int b) throws RemoteException {
+        if(b==0) {return 0;}
+        else {return (float) a/b;}
+    }
+
+    public static void main(String args[]) {
+        try {
+            //puerto default del rmiregistry
+            java.rmi.registry.LocateRegistry.createRegistry(1099);
+            System.out.println("RMI registro listo.");
+        } catch (Exception e) {
+            System.out.println("Excepcion RMI del registry:");
+            e.printStackTrace();
+        }//catch
+        try {
+            //System.setProperty("java.rmi.server.codebase","file:/c:/Temp/Suma/"); windows
+            // en linux lo hace en auto
+            Servidor obj = new Servidor();
+            Calculadora stub = (Calculadora) UnicastRemoteObject.exportObject(obj, 0);
+            // Ligamos el objeto remoto en el registro
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("Calculadora", stub);
+
+            System.err.println("Servidor listo...");
+        } catch (Exception e) {
+            System.err.println("Excepción del servidor: " + e.toString());
+            e.printStackTrace();
+        }
+    }
 }
