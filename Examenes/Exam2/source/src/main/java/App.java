1845ed8d1e689e44217ffaf969a3d1043840e5af
AaronGG11 <aarongarcia.ipn.escom@gmail.com>
2020-12-13 10:24:14 -0600

    Solitario OK - falta boton iniciar

--- Examenes/Exam2/source/src/main/java/App.java
+++ Examenes/Exam2/source/src/main/java/App.java
@@ -1,10 +1,10 @@
 import Utilidades.Memorama;
 
 import javax.swing.*;
+import java.time.LocalTime;
 
 
 public class App {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
     }
 }
