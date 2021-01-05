554559bb4b6f658ce50371564316adc240be493f
AaronGG1 <aarongarcia.ipn.escom@gmail.com>
2021-01-04 22:08:18 -0600

    Subiendo todos los emojis

--- Practicas/ChatMulticast/ChatSocketsMultidifusion/ChatMulticast/src/serviciodechat/AnalisisDeMensajes.java
+++ Practicas/ChatMulticast/ChatSocketsMultidifusion/ChatMulticast/src/serviciodechat/AnalisisDeMensajes.java
@@ -7,29 +7,14 @@ package serviciodechat;
  */
 public class AnalisisDeMensajes 
 {
-    // Alegre -> 
-    private final String ALEGRE = " <img src=" +
-                                    "http://tusimagenesde.com/wp-content/uploads/2015/01/gifs-animados-5.gif"
-                                    + " width='30' height='30' alt='emoji'> ";
-    
-    // Lengua
-    private final String LENGUA = " <img src=" +this.getClass().getClassLoader()
-                                    .getResource("Emojis/lengua.png").toString() 
-                                    + " width='30' height='30' alt='emoji'> ";
-    
-    // Triste -> 
-    private final String TRISTE = " <img src=" +this.getClass().getClassLoader()
-                                    .getResource("Emojis/triste.png").toString() 
-                                    + " width='30' height='30' alt='emoji'> ";
+    // Risa -> :)
+    private final  String RISA = " <img src=" +this.getClass().getClassLoader()
+            .getResource("Emojis/risa.png").toString()
+            + " width='30' height='30' alt='emoji'> ";
     
     // Amor -> :o
      private final String AMOR = " <img src=" +this.getClass().getClassLoader()
                                     .getResource("Emojis/amor.png").toString() 
-                                    + " width='30' height='30' alt='emoji'> ";
-     
-    // Risa -> :) * xd
-    private final  String RISA = " <img src=" +this.getClass().getClassLoader()
-                                    .getResource("Emojis/risa.png").toString() 
                                     + " width='30' height='30' alt='emoji'> ";
     
     // Enojo -> :S
@@ -47,8 +32,7 @@ public class AnalisisDeMensajes
                                     "http://tusimagenesde.com/wp-content/uploads/2015/01/gifs-animados-5.gif"
                                     + " width='30' height='30' alt='emoji'> ";
     
-    
-    
+
     public String formatoAMensaje(String mensaje)
     {
         int tam = mensaje.length();
@@ -60,37 +44,13 @@ public class AnalisisDeMensajes
             c = mensaje.charAt(i);
             if(c == ' ')
             {   
-                c = mensaje.charAt(i + 1); 
-                if( c == 'x')
-                {   //risa
-                    if(mensaje.charAt(i + 2) == 'd' && mensaje.charAt(i + 3) == ' ')
-                    {
-                        msj += ALEGRE;
-                        i += 3;
-                        System.out.print("imgr");
-                    }
-                    else
-                        msj += ' ';
-                }
-                else if(c == ':')
+                c = mensaje.charAt(i + 1);
+                if(c == ':')
                 {
-                    
                     if(mensaje.charAt(i + 3) == ' ')
                     {
                         c = mensaje.charAt(i + 2);
-                        if(c == 'p')
-                        {
-                            msj += LENGUA;
-                            i += 3;
-                            System.out.print("imgl");
-                        }
-                        else if(c == '(')
-                        {
-                           msj += TRISTE;
-                            i += 3;
-                            System.out.print("imgt");
-                        }
-                        else if(c == ')')
+                        if(c == ')')
                         {
                            msj += RISA;
                             i += 3;
@@ -101,18 +61,52 @@ public class AnalisisDeMensajes
                             i += 3;
                             System.out.print("imga");
                         }
+                        else if(c == 'S'){
+                            msj += ENOJO;
+                            i += 3;
+                            System.out.print("imge");
+                        }
                         else
                             msj += ' ';
                     }
                     else
                         msj += ' ';
                 }
+                else if(c == '_'){
+                    if(mensaje.charAt(i + 6) == '_')
+                    {
+                        if((mensaje.charAt(i + 2) == 'l')&&
+                                (mensaje.charAt(i + 3) == 'o')&&
+                                (mensaje.charAt(i + 4) == 'c')&&
+                                (mensaje.charAt(i + 5) == 'o')){
+
+                            msj += LOCO;
+                            i += 7;
+                            System.out.print("imgc");
+                        }
+                    }
+                    else if(mensaje.charAt(i + 8) == '_'){
+                        if((mensaje.charAt(i + 2) == 'H')&&
+                                (mensaje.charAt(i + 3) == 'o')&&
+                                (mensaje.charAt(i + 4) == 'm')&&
+                                (mensaje.charAt(i + 5) == 'e')&&
+                                (mensaje.charAt(i + 6) == 'r')&&
+                                (mensaje.charAt(i + 7) == 'o')){
+
+                            msj += HOMERO;
+                            i += 9;
+                            System.out.print("imgh");
+                        }
+                    }else{
+                        msj += ' ';
+                    }
+                }
                 else
                     msj += ' ';
             }
             else
                 msj += c;
-             
+
             i++;
         }
         return "<html> " + msj + "</html>";
